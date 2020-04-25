983fa1936a1f371521855560423af185dc022794
annabauza <31966073+annabauza@users.noreply.github.com>
2020-04-25 19:48:38 +0100

    Updates

---
+++ precache-manifest.b1457dd8cba30f06b6a7726169fff88e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5f6f256305d767c900027af31f62c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14e7e2b3b64232abc751",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6233a875a2dd4443e099",
+    "url": "/static/js/2.a6c5ff91.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/static/js/2.a6c5ff91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e7e2b3b64232abc751",
+    "url": "/static/js/main.94d51883.chunk.js"
+  },
+  {
+    "revision": "31805412c6945a3310f6",
+    "url": "/static/js/runtime-main.fdfdc0bb.js"
+  },
+  {
+    "revision": "45cc4ce0406ddd458e03a61f586ea483",
+    "url": "/static/media/logo.45cc4ce0.png"
+  }
+]);
