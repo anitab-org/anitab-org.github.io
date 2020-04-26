424efc321f4b5b4d9e01036e603bdd1546df64b9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-26 11:29:30 +0000

    Deploy anitab-org/anitab-org.github.io to github.com/anitab-org/anitab-org.github.io.git:master

---
+++ precache-manifest.301e3001a762690af53e26b117531f83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0f86a38ad5e2addee3f395c728db7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5defb5954cbb40d0a56e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1cf8e0022bf3acece095",
+    "url": "/static/js/2.9460ca0e.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/static/js/2.9460ca0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5defb5954cbb40d0a56e",
+    "url": "/static/js/main.d4bafb34.chunk.js"
+  },
+  {
+    "revision": "d02f182dd9c863612c01",
+    "url": "/static/js/runtime-main.a8d8665d.js"
+  },
+  {
+    "revision": "552c358973068e62c7a41bbbcce2fc67",
+    "url": "/static/media/contribute.552c3589.png"
+  },
+  {
+    "revision": "45cc4ce0406ddd458e03a61f586ea483",
+    "url": "/static/media/logo.45cc4ce0.png"
+  }
+]);
