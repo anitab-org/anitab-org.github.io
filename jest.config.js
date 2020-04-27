196d81dd143ac6a0cec15a5de24c2d9658b903a2
gaurivn <gaurivgn@gmail.com>
2020-04-27 14:18:17 +0400

    undo changes

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  verbose: true,
-  setupFilesAfterEnv: ['./src/setupTests.js'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.css$': 'jest-transform-css',
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': 'jest-transform-file'
-  }
-};
