538d9a6f3c982c8a38e6347a8af5f6545357b461
Souvik <souvikde.ns@gmail.com>
2020-07-01 20:09:15 +0530

    pr for issue #85

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import SectionHeader from './../SectionHeader';
 import Sponsors from './Sponsors';
 
@@ -15,6 +15,48 @@ function Projects() {
     >
       {/* <SectionHeader title="Partners" />
       <Sponsors /> */}
+      <View
+        style={{
+          marginTop: '1.2rem',
+          flex: 1,
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-around'
+        }}
+      >
+        <View 
+        style={{
+          width: '500px'
+        }}
+        >
+          <Text
+            style={{ fontSize: '2.2rem', color: '#416891'}}
+          >Projects</Text>
+
+          <Text
+            style={{ color: '#4c7db0', marginTop: '.5rem'}}
+          >
+          At AnitaB.org, we envision a future where the people who imagine and build technology 
+          mirror the people and societies for whom they build it. We connect, inspire, and guide 
+          women in computing, and organizations that view technology innovation as a strategic 
+          imperative.
+          </Text>
+
+          <Text>
+            
+          </Text>
+        </View>
+
+        <View 
+        style={{width: '50%'}}
+        >
+          <Image
+            style={{ height: 300, width: '100%' }}
+            source={require('../../assets/project.svg')}
+          />
+        </View>
+
+      </View>
     </View>
   );
 }
