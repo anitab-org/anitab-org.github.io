a07df82faf9d643a38ac6bc43c883cb4f773f338
Souvik <souvikde.ns@gmail.com>
2020-07-02 19:55:03 +0530

    Update index.js

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -24,31 +24,30 @@ function Projects() {
           justifyContent: 'space-around'
         }}
       >
-        <View 
-        style={{
-          width: '500px'
-        }}
+        <View
+          style={{
+            width: '500px'
+          }}
         >
           <Text
-            style={{ fontSize: '2.2rem', color: '#416891'}}
+            style={{ fontSize: '2.2rem', color: '#416891' }}
           >Projects</Text>
 
           <Text
-            style={{ color: '#4c7db0', marginTop: '.5rem'}}
+            style={{ color: '#4c7db0', marginTop: '.5rem' }}
           >
-          At AnitaB.org, we envision a future where the people who imagine and build technology 
-          mirror the people and societies for whom they build it. We connect, inspire, and guide 
-          women in computing, and organizations that view technology innovation as a strategic 
-          imperative.
-          </Text>
-
-          <Text>
-            
+            Anita Borg the famous computer scientist said,"<strong>
+              We'll try to be a catalyst for development projects that include women at all levels - in the design and development process
+              </strong>."
+            At AnitaB.org, we envision a future where the people who imagine and build technology mirror the people and societies for whom they build it. We connect, inspire, and guide women in computing, and organizations that view technology innovation as a strategic imperative.
+            <strong>
+              Our team under the guidance of our admins is working together to make some amazing projects. Scroll down to know about it and if interested start contributing to them.
+            </strong>
           </Text>
         </View>
 
-        <View 
-        style={{width: '50%'}}
+        <View
+          style={{ width: '50%' }}
         >
           <Image
             style={{ height: 300, width: '100%' }}
