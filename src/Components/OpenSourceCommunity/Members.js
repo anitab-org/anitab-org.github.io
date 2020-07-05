8f48a76cdfd2843d8f668ca31f34022036a34cb5
anna4j <anushkajadhav004@gmail.com>
2020-07-05 16:34:57 +0530

    feat: Members add

--- src/Components/OpenSourceCommunity/Members.js
+++ src/Components/OpenSourceCommunity/Members.js
@@ -1,48 +1,59 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
     View,
-    Text,
     FlatList,
-    ActivityIndicator,
     Image,
     Linking,
     TouchableHighlight,
-    ScrollView
 } from 'react-native';
 import styles from './styles';
 import { Dimensions } from "react-native";
-import content from './Data/index.json';
-
+import Data from './Data.js';
 
 
 //import all the components we will need
 
 export default class Members extends Component {
+
     constructor() {
         super();
-        this.state = {
-            dataSource: {},
-        };
-    }
-    constructor2() {
-        //super();
         this.state = { screenWidth: "" }
     }
+
     getScreenSize = () => {
         const screenWidth = Math.round(Dimensions.get('window').width);
         this.setState({ screenWidth: screenWidth })
     }
     componentDidMount() {
         var that = this;
-        let items = Array.apply(null, Array(25)).map((v, i) => {
-            return { id: i, src: 'https://avatars1.githubusercontent.com/u/42520146?s=400&u=a64745b8570c25a709ec1bcb08fba621732ee017&v=4'/*`./images/${i + 1}.jpg`*/ };
-        });
+        let items = [
+            {
+                profile: "https://github.com/aayush-05",
+                image: "https://avatars1.githubusercontent.com/u/47032027?s=96&v=4"
+            },
+            {
+                profile: "https://github.com/Abdelaty",
+                image: "https://avatars0.githubusercontent.com/u/23483020?s=96&v=4"
+            },
+            {
+                profile: "https://github.com/adi-001",
+                image: "https://avatars3.githubusercontent.com/u/47720423?s=96&v=4"
+            },
+            {
+                profile: "https://github.com/aditmehta9",
+                image: "https://avatars2.githubusercontent.com/u/35133994?s=96&v=4"
+            }
+        ];
+
         that.setState({
             dataSource: items,
         });
     }
+
+
     render() {
+
+
         return (
             <View style={styles.MainContainer}>
                 <FlatList
@@ -54,23 +65,16 @@ export default class Members extends Component {
                                 style={styles.link}
                                 underlayColor='transparent'
                                 accessible={true}
-                                onPress={() => Linking.openURL('https://anitab.org/about-us/about-anita-borg/')}
+                                onPress={() => Linking.openURL(item.profile)}
                             >
-                                <Image style={styles.imageThumbnail} source={{ uri: item.src }} />
+                                <Image style={styles.imageThumbnail} source={item.image} />
                             </TouchableHighlight>
 
                         </View>
                     )}
-                    //Setting the number of column
-                    //numColumns={20}
-
-                    //numColumns={this.state.screenWidth / 61}
-
-                    keyExtractor={item => item.id/*(item, index) => index.toString()*/}
+                    keyExtractor={item => item.id}
                 />
             </View>
         );
     }
 }
-
-
