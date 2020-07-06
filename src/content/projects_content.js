33b5d493a1dd90c6d9a14f60b9c7d14c2c73079b
gaurivn <gaurivgn@gmail.com>
2020-07-07 01:03:24 +0400

    changed to .js

---
+++ src/content/projects_content.js
@@ -0,0 +1,179 @@
+const projects = {
+    sections: [
+      {
+        title: "Projects",
+        cards: [
+          {
+            title: "Mentorship Android",
+            description: "Android client for Mentorship System - an application that allows women in tech to mentor each other, on career development topics, through 1:1 relations for a certain period.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                
+              },
+              {
+                title: "github",
+               
+              }
+            ]
+          },
+          
+          {
+            title: "Mentorship Backend",
+            description: "Backend REST API for Mentorship System - an application that allows women in tech to mentor each other, on career development topics, through 1:1 relations for a certain period.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                 
+              },
+              {
+                title: "github",
+               
+              }
+            ]
+          },
+
+          {
+            title: "Mentorship iOS",
+            description: "iOS application for Mentorship System - an application that matches women in tech to mentor each other, on career development, through 1:1 relations during a certain period of time.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                 
+              },
+              {
+                title: "github",
+                
+              }
+            ]
+          },
+
+          {
+            title: "Portal",
+            description: "AnitaB.org Portal is for AnitaB.org communities to post and share information within and with other communities.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                
+              },
+              {
+                title: "github",
+                
+              }
+            ]
+          },
+
+          {
+            title: "VMS",
+            description: "AnitaB.org has many volunteers that offer their skills, time, and commitment to our vision and projects. The Volunteer Management System (VMS) will be useful for volunteer coordinators, volunteer sign-up, tracking hours, and reporting. This project uses the Django web framework and Python.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                 
+              },
+              {
+                title: "github",
+                
+              }
+            ]
+          },
+
+          {
+            title: "AnitaB.org Open Source Website",
+            description: "AnitaB.org Open Source web gathering all informations about current open source projects, running programs and events as well as all necessary contribution informations.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                 
+              },
+              {
+                title: "github",
+                
+              }
+            ]
+          },
+
+          {
+            title: "Bridge-In-Tech",
+            description: "Bridge-In-Tech is an application that allows industries/companies, mentors and students to actively collaborate to one another.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                
+              },
+              {
+                title: "github",
+                
+              }
+            ]
+          },
+
+          {
+            title: "Open Source Programs",
+            description: "Open Source Programs (OSP) is an application that simplifies the processing and selection procedure of Open Source Programs of AnitaB.org Open Source or other third-party programs.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                
+              },
+              {
+                title: "github",
+                
+              }
+            ]
+          },
+
+          {
+            title: "Stem Diverse Android TV",
+            description: "STEM Diverse TV is an AndroidTV application aims to promote diversity in the field of STEM by inspiring women so as to leverage their interest in the field.",
+            labels: [
+              "javascript",
+              "mvvm"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                
+              },
+              {
+                title: "github",
+                
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
