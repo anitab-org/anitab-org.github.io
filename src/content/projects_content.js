19385478ccf5873c1e1f6ae974684b82af1600c2
gaurivn <gaurivgn@gmail.com>
2020-07-23 22:03:46 +0400

    updated images, badges, urls

--- src/content/projects_content.js
+++ src/content/projects_content.js
@@ -7,17 +7,19 @@ const projects = {
             title: "Mentorship Android",
             description: "Android client for Mentorship System - an application that allows women in tech to mentor each other, on career development topics, through 1:1 relations for a certain period.",
             labels: [
-              "javascript",
+              "kotlin",
               "mvvm"
             ],
             badges: [
               {
                 title: "zulip",
-                
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/222534-mentorship-system/topic/Android',
               },
               {
                 title: "github",
-               
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/mentorship-android',
               }
             ]
           },
@@ -26,17 +28,19 @@ const projects = {
             title: "Mentorship Backend",
             description: "Backend REST API for Mentorship System - an application that allows women in tech to mentor each other, on career development topics, through 1:1 relations for a certain period.",
             labels: [
-              "javascript",
-              "mvvm"
+              "flask",
+              "python"
             ],
             badges: [
               {
                 title: "zulip",
-                 
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/222534-mentorship-system/topic/mentorship-backend',
               },
               {
                 title: "github",
-               
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/mentorship-backend',
               }
             ]
           },
@@ -45,17 +49,19 @@ const projects = {
             title: "Mentorship iOS",
             description: "iOS application for Mentorship System - an application that matches women in tech to mentor each other, on career development, through 1:1 relations during a certain period of time.",
             labels: [
-              "javascript",
+              "swiftUI",
               "mvvm"
             ],
             badges: [
               {
                 title: "zulip",
-                 
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/222534-mentorship-system/topic/iOS',
               },
               {
                 title: "github",
-                
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/mentorship-ios',
               }
             ]
           },
@@ -64,17 +70,19 @@ const projects = {
             title: "Portal",
             description: "AnitaB.org Portal is for AnitaB.org communities to post and share information within and with other communities.",
             labels: [
-              "javascript",
-              "mvvm"
+              "django",
+              "python"
             ],
             badges: [
               {
                 title: "zulip",
-                
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/222540-portal',
               },
               {
                 title: "github",
-                
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/portal',
               }
             ]
           },
@@ -83,17 +91,19 @@ const projects = {
             title: "VMS",
             description: "AnitaB.org has many volunteers that offer their skills, time, and commitment to our vision and projects. The Volunteer Management System (VMS) will be useful for volunteer coordinators, volunteer sign-up, tracking hours, and reporting. This project uses the Django web framework and Python.",
             labels: [
-              "javascript",
-              "mvvm"
+              "django",
+              "python"
             ],
             badges: [
               {
                 title: "zulip",
-                 
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/222539-vms',
               },
               {
                 title: "github",
-                
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/vms',
               }
             ]
           },
@@ -102,74 +112,122 @@ const projects = {
             title: "AnitaB.org Open Source Website",
             description: "AnitaB.org Open Source web gathering all informations about current open source projects, running programs and events as well as all necessary contribution informations.",
             labels: [
-              "javascript",
-              "mvvm"
+              "react-native",
+              "javascript"
             ],
             badges: [
               {
                 title: "zulip",
-                 
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/235478-anitab-org.2Egithub.2Eio',
               },
               {
                 title: "github",
-                
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/anitab-org.github.io',
               }
             ]
           },
 
           {
-            title: "Bridge-In-Tech",
-            description: "Bridge-In-Tech is an application that allows industries/companies, mentors and students to actively collaborate to one another.",
+            title: "Bridge-In-Tech Web",
+            description: "Frontend of Bridge-In-Tech - an application that allows industries/companies, mentors and students to actively collaborate to one another.",
             labels: [
               "javascript",
-              "mvvm"
             ],
             badges: [
               {
                 title: "zulip",
-                
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/224798-gsoc/topic/BridgeInTech.20-.20Maya',
               },
               {
                 title: "github",
-                
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/bridge-in-tech-web',
               }
             ]
           },
 
           {
-            title: "Open Source Programs",
-            description: "Open Source Programs (OSP) is an application that simplifies the processing and selection procedure of Open Source Programs of AnitaB.org Open Source or other third-party programs.",
+            title: "Bridge-In-Tech Backend",
+            description: "Backend of Bridge-In-Tech - an application that allows industries/companies, mentors and students to actively collaborate to one another.",
             labels: [
-              "javascript",
-              "mvvm"
+              "flask-restx",
+              "python"
             ],
             badges: [
               {
                 title: "zulip",
-                
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/224798-gsoc/topic/BridgeInTech.20-.20Maya',
               },
               {
                 title: "github",
-                
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/bridge-in-tech-backend',
               }
             ]
           },
 
           {
-            title: "Stem Diverse Android TV",
-            description: "STEM Diverse TV is an AndroidTV application aims to promote diversity in the field of STEM by inspiring women so as to leverage their interest in the field.",
+            title: "Open Source Programs Web",
+            description: "Frontend of Open Source Programs (OSP) - an application that simplifies the processing and selection procedure of Open Source Programs of AnitaB.org Open Source or other third-party programs.",
             labels: [
               "javascript",
-              "mvvm"
             ],
             badges: [
               {
                 title: "zulip",
-                
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/224798-gsoc/topic/Open.20Source.20Programs.20-.20Bismita.20Guha',
               },
               {
                 title: "github",
-                
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/open-source-programs-web',
+              }
+            ]
+          },
+
+          {
+            title: "Open Source Programs Backend",
+            description: "Backend of Open Source Programs (OSP) - an application that simplifies the processing and selection procedure of Open Source Programs of AnitaB.org Open Source or other third-party programs.",
+            labels: [
+              "django",
+              "python"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/224798-gsoc/topic/Open.20Source.20Programs.20-.20Bismita.20Guha',
+              },
+              {
+                title: "github",
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/open-source-programs-backend',
+              }
+            ]
+          },
+
+          {
+            title: "Stem Diverse TV",
+            description: "STEM Diverse TV is an app which provides inspiring, motivating, supportive, educational and informative videos to promote and increase diversity in STEM.",
+            labels: [
+              "flask",
+              "python"
+            ],
+            badges: [
+              {
+                title: "zulip",
+                source: require('./../assets/zulip.png'),
+                url: 'https://anitab-org.zulipchat.com/#narrow/stream/225705-STEM-diverse-tv',
+              },
+              {
+                title: "github",
+                source: require('./../assets/github.png'),
+                url: 'https://github.com/anitab-org/stem-diverse-tv',
               }
             ]
           }
