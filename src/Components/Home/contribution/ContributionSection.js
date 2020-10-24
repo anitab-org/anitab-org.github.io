66f01f07ae34b7d8a171b1c51a0845cc9f3a5c02
Anna Bauza <31966073+annabauza@users.noreply.github.com>
2020-10-24 17:11:42 +0100

    Update ContributionSection.js

--- src/Components/Home/contribution/ContributionSection.js
+++ src/Components/Home/contribution/ContributionSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View } from 'react-native';
 import styles from '../styles';
 import SectionHeader from '../../SectionHeader';
 import ContributionCard from './ContributionCard';
