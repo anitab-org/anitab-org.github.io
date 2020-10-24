2c65e270a9a74c1e1c6c14d01fc568a07b4c8dc6
annabauza <31966073+annabauza@users.noreply.github.com>
2020-10-24 15:57:24 +0100

    fix: footer issues

--- src/Components/Footer/styles.js
+++ src/Components/Footer/styles.js
@@ -2,43 +2,46 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     backgroundColor: '#231B4E',
     width: '100%',
     flexWrap: 'wrap',
+    marginTop: 32,
     paddingBottom: 16,
-    paddingTop: 24,
-    paddingHorizontal: '10%'
+    paddingHorizontal: '10%',
   },
   col: { alignItems: 'center' },
-  left: { width: 579 },
-  center: { width: 245 },
+  left: { flex: 1, paddingTop: 24, minWidth: 300 },
+  center: { flex: 1, paddingTop: 24, minWidth: 300 },
   right: {
-    width: 573,
-    textAlign: 'center'
+    flex: 1,
+    paddingTop: 24,
+    minWidth: 300,
+    textAlign: 'center',
   },
   text: { color: '#FFFFFF' },
   heading: {
-    fontSize: '1.4em',
+    fontSize: 18,
     fontWeight: 500,
-    paddingBottom: 22
+    paddingBottom: 22,
   },
   description: {
-    fontSize: '1em',
-    fontWeight: 300
+    fontSize: 14,
+    fontWeight: 300,
   },
   formField: {
     backgroundColor: '#FFFFFF',
     width: '100%',
     borderRadius: 8,
     padding: 4,
-    borderColor: 'red'
+    borderColor: 'red',
   },
   formEmail: { height: 38 },
   formMessage: {
     height: 161,
-    marginVertical: 8
+    marginVertical: 8,
   },
   formButton: {
     width: 183,
@@ -46,15 +49,15 @@ const styles = StyleSheet.create({
     borderRadius: 500,
     backgroundColor: '#54BCEB',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   socialContainer: { flexDirection: 'row' },
   socialImage: {
     height: 32,
     width: 32,
     borderRadius: '50%',
-    marginHorizontal: 16
-  }
+    marginHorizontal: 16,
+  },
 });
 
 export default styles;
