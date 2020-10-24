28e78ea16d320e7560f5244fc6d2f76e5241caba
Ria Dhanani <ria08.dhanani@gmail.com>
2020-10-24 20:40:09 +0530

    Add home section (#125)

--- src/Components/Home/styles.js
+++ src/Components/Home/styles.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { styled } from 'react-native-reflect';
 
 export const Box = styled(View, {
@@ -26,3 +26,45 @@ export const Description = styled(Text, {
   color: '#103B81',
   textAlign: 'left',
 });
+
+const styles = StyleSheet.create({
+    container: {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      marginTop: 18,
+      marginBottom: 18,
+    },
+    card:{
+      width: '33%',
+    },
+    cardMiddle: {
+      alignItems: "center"
+    },
+    cardLast: {
+      alignItems: 'flex-end'
+    },
+    contributionImage: {
+      width: 235,
+      height: 235
+    },
+    imageText: {
+      fontSize: 14,
+      fontWeight: '300',
+      fontFamily: 'Avenir',
+      textAlign: 'center',
+      width: 180,
+      flex: 1,
+      color: '#103B81'
+    },
+    description: {
+      fontSize : 14,
+      fontWeight: '300',
+      color: '#103B81',
+      marginLeft: 18,
+      }
+    
+  
+  });
+
+export default styles;
