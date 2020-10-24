b361d673f3a2e27e320b20b3c3efdc597916a076
annabauza <31966073+annabauza@users.noreply.github.com>
2020-10-24 17:04:33 +0100

    remove form import

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import styles from './styles';
-import Form from './Form';
+// import Form from './Form';
 import Social from './Social';
 import Contact from './Contact';
 
