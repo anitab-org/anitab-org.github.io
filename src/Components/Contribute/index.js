7e540a5c69fc51e8986e24f78fef7f954774ed81
Rutvi Sharma <43277445+rutvi18@users.noreply.github.com>
2020-11-20 10:43:22 +0530

    import contibuteintro

--- src/Components/Contribute/index.js
+++ src/Components/Contribute/index.js
@@ -1,36 +1,43 @@
 import React from 'react';
 import SectionHeader from '../SectionHeader';
-import content from '../../content/contributeIntro.json';
+//import content from './../../content/contributeIntro.json';
 import ImageContent from './../ImageContent';
 import { MainContainer, Box, Content, Description } from './style';
+import { getcontributeIntro } from '../../content/contributeintro';
+
 
 function Contribute() {
+    const content = getcontributeIntro();
     const renderContent = () => {
         return (
             <Box>
                 {content.sections.map((section, index) => {
                     return (
-                        <Content>
+                        <Content key={index}>
                             <SectionHeader title={section.title} />
                             {section.content.map((content) => {
                                 return <Description>{content.par}</Description>;
                             })}
-                        </Content>
+                            
+                         </Content>
+                        
                     );
                 })}
             </Box>
         );
     };
-
-    return (
+return (
         <MainContainer>
             <ImageContent
                 image={require('../../assets/Frame 1.png')}
                 imageSide="right"
                 Children={renderContent}
+                
             />
+            
         </MainContainer>
     );
 }
+
 
 export default Contribute;
