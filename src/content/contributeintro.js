53e2ac8d19e033b914afd1a42d5055479e13fbfd
Rutvi Sharma <43277445+rutvi18@users.noreply.github.com>
2020-11-20 18:41:44 +0530

    change_2

---
+++ src/content/contributeintro.js
@@ -0,0 +1,46 @@
+const contributeIntro = {
+    sections: [
+      {
+        title: 'Code',
+        image: {
+          source: require('./../assets/contribution/code.png'),
+        },
+        content: [
+          {
+            par:
+              'hegjfgwj ',
+          },
+          {
+            par:
+              "dgfjgrj",
+          },
+          {
+            par:
+              "rfhrhfrje",
+          },
+        ],
+      },
+      {
+        title: 'Documentation/Training',
+        image: {
+          source: require('./../assets/contribution/documentation_training.png'),
+        },
+        content: [
+          {
+            par:
+              'dfbhsdjs',
+          },
+          {
+            par:
+              "jhgfjgrrj",
+          },
+        ],
+      },
+    ],
+} ,
+
+  
+  export const getcontributeIntro = () => {
+    return contributeIntro;
+  };
+  
