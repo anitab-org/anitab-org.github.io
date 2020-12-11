a37513f73d3e1546a0ac8bf3ba747f36716315d2
shravani05 <43898702+shravani05@users.noreply.github.com>
2020-12-12 00:05:06 +0530

    Added padding and sectionSubheader component to Events page

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import SectionSubheader from '../SectionSubheader';
 import GoogleCalendar from './GoogleCalendar';
 
 function Events() {
-  return (
-    <View style={styles.container}>
-      <Text h1 style={styles.heading}>EVENT CALENDAR</Text> 
+  return (  
+      <View style={styles.container}>
+      <SectionSubheader title="Event Calendar"/>
       <Text style={styles.data}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada massa nec porta interdum. 
       Vivamus porta mollis urna at congue. 
@@ -14,7 +15,7 @@ function Events() {
       Donec vestibulum ullamcorper sapien et egestas. Ut eu aliquet leo.
       </Text>
       <GoogleCalendar/>
-    </View>
+      </View>
   );
 }
 
@@ -25,13 +26,11 @@ const styles = StyleSheet.create({
     paddingLeft: 16,
     paddingRight: 16,
   },
-  heading:{
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "#013287"
-  },
   data:{
-    color: "#013287"
+    color: '#103B81',
+    paddingLeft: 16,
+    paddingBottom: 25,
+    paddingTop: 16
   }
 });
 
