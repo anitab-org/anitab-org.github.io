4d18c14ed82380b40bf15ba09b90d68b322dd2be
shravani05 <43898702+shravani05@users.noreply.github.com>
2020-12-13 01:31:28 +0530

    Added style to the Events page as per the other pages.

--- src/Components/Events/GoogleCalendar/index.js
+++ src/Components/Events/GoogleCalendar/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { StyleSheet, View} from 'react-native';
+import { MainContainer } from '../style';
 
 function GoogleCalendar() {
   return (
-    <View style={styles.container}>
+    <MainContainer>
         {/* Replace this iframe with the iframe of the google calendar associated with the google account according to which you want the schedule */}
         <iframe src="https://calendar.google.com/calendar/embed?src=er3gi62a5hip5jnk1f7r0al62s%40group.calendar.google.com&ctz=America%2FLos_Angeles" 
           title="Calendar" style={{border: "0", width: "100%", height:"500px", frameborder:"0", scrolling:"no"}}>
         </iframe>
-    </View>
+    </MainContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "80%",
-    alignItems: 'left',
-    paddingLeft: 16,
-    paddingRight: 16,
-  }
-});
 
 export default GoogleCalendar; 
--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 import SectionSubheader from '../SectionSubheader';
 import GoogleCalendar from './GoogleCalendar';
+import { Box, MainContainer, Content, Description } from './style';
 
 function Events() {
+
+  const renderCalendar = () => {
+    return(
+      <Box>
+        <Content>
+          <SectionSubheader title="Event Calendar"/>
+          <Description>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada massa nec porta interdum. 
+          Vivamus porta mollis urna at congue. 
+          Maecenas mollis, dolor ut auctor ultrices, diam ex dignissim ante, at sollicitudin risus risus id felis. 
+          Suspendisse vel ante placerat, feugiat ligula pretium, dignissim est. 
+          Donec vestibulum ullamcorper sapien et egestas. Ut eu aliquet leo.
+          </Description>
+          <GoogleCalendar/>
+        </Content>
+      </Box>
+    )
+  }
+
   return (  
-      <View style={styles.container}>
-      <SectionSubheader title="Event Calendar"/>
-      <Text style={styles.data}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada massa nec porta interdum. 
-      Vivamus porta mollis urna at congue. 
-      Maecenas mollis, dolor ut auctor ultrices, diam ex dignissim ante, at sollicitudin risus risus id felis. 
-      Suspendisse vel ante placerat, feugiat ligula pretium, dignissim est. 
-      Donec vestibulum ullamcorper sapien et egestas. Ut eu aliquet leo.
-      </Text>
-      <GoogleCalendar/>
-      </View>
+    <MainContainer>
+      {renderCalendar()}
+    </MainContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "80%",
-    alignItems: 'left',
-    paddingLeft: 16,
-    paddingRight: 16,
-  },
-  data:{
-    color: '#103B81',
-    paddingLeft: 16,
-    paddingBottom: 25,
-    paddingTop: 16
-  }
-});
 
 export default Events;
