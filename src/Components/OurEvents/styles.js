91bcfc8d1487f65983a45f91255bd867bd1b9583
Prabhat Sharma <prabhat.04011998@gmail.com>
2020-12-20 21:38:57 +0530

    Conflicts resolved

--- src/Components/OurEvents/styles.js
+++ src/Components/OurEvents/styles.js
@@ -46,6 +46,10 @@ const styles = StyleSheet.create({
   cardLast: {
     alignItems: 'flex-end',
   },
+  contributionImage: {
+    width: 235,
+    height: 235,
+  },
   imageText: {
     fontSize: 14,
     fontWeight: '300',
@@ -59,7 +63,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '300',
     color: '#103B81',
-    marginLeft: 32,
+    marginLeft: 18,
   },
 });
 
