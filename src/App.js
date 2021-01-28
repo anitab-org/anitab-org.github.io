eb12d332d7e2eeddcb9605983c8f8d3fd222ecb5
Sagar Barapatre <b119016@iiit-bh.ac.in>
2021-01-28 10:50:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import { View } from 'react-native';
 import Header from './Components/Header';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./Components/globalStyles";
+import { lightTheme, darkTheme } from "./Components/Theme";
+// import { useDarkMode } from "./Components/useDarkMode";
+// import Toggle from "./Components/Toggler"
 
 function App() {
   const [selected, setSelected] = useState(0);
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+ 
   const titles = [
     'HOME',
     'ABOUT US',
@@ -15,12 +25,20 @@ function App() {
     'CONTRIBUTE'
   ];
   return (
-    <View style={{ position: 'absolute', width: '100%', alignItems: 'center' }}>
-      <Header selected={selected} setSelected={setSelected} titles={titles} />
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+   
+      <>
+      <GlobalStyles/>
+        <View style={{ position: 'absolute', width: '100%', alignItems: 'center' }}>
+          
+          <Header selected={selected} setSelected={setSelected} titles={titles} />
+          <button onClick={themeToggler}>Switch Theme</button>
+          
       <Content selected={selected} titles={titles} />
       <Footer />
     </View>
-  );
+  </>
+    </ThemeProvider>);
 }
 
 export default App;
