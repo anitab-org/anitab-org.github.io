2a16b21043fe41ef2c65542761aa49f31b66d377
Anna Bauza <31966073+annabauza@users.noreply.github.com>
2021-02-05 19:27:53 +0000

    Update src/content/our_events.js

--- src/content/our_events.js
+++ src/content/our_events.js
@@ -8,7 +8,7 @@ const our_events={
             content: [
                 {
                     par : "We at AnitaB.org Open Source believe in Inspiration through Action. To facilitate our Open Source journey and continue the AnitaB.org legacy we conduct enthralling events all year long, with the purpose of bridging the gap between creative developers and our Open Source community. Keep scrolling to catch a glimpse of few such events! ",
-                }
+                },
                 
                 {
                     par: "We at AnitaB.org Open Source believe in Inspiration through Action. To facilitate our Open Source journey and continue the AnitaB.org legacy we conduct enthralling events all year long, with the purpose of bridging the gap between creative developers and our Open Source community. Keep scrolling to catch a glimpse of few such events! ",
