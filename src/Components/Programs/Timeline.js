e2e820bcecf92349f50192ae15144a10e7f1e46d
Prathamesh bhalekar <prathameshbhalekar13@gmail.com>
2021-02-12 08:38:02 +0530

    Fix : Place Footer at bottom and Timeline at center in programs tab

--- src/Components/Programs/Timeline.js
+++ src/Components/Programs/Timeline.js
@@ -1,7 +1,7 @@
 import React, {useEffect,useRef} from 'react';
 import dayjs from 'dayjs';
 import {StyleSheet,Text,View,TouchableWithoutFeedback,ScrollView} from 'react-native';
-import {Event,Line,Date,Marker,Fade,Stroke,ArrowNavigation,ArrowText,ScrollContainer,Container,Months} from './styles';
+import {Event,Line,Date,Marker,Fade,Stroke,MainContainer,ArrowNavigation,ArrowText,ScrollContainer,Container,Months} from './styles';
 import {ArrowLeftOutlined ,ArrowRightOutlined} from '@ant-design/icons'
 import {getEvents,getMonths} from '../../content/programs_events'
 //--------------------------------------------------------------------------------------------
@@ -34,82 +34,84 @@ function Timeline() {
     }
   return (
       <>
-            <ArrowNavigation>
-                <View style={{flexDirection:'row'}}>
-                    <TouchableWithoutFeedback onPress={scrollLeft}>
-                        <ArrowLeftOutlined style={{color:'#42aaf5'}}/>
-                    </TouchableWithoutFeedback>
-                    <ArrowText>PAST</ArrowText>
-                </View>
-                <View style={{flexDirection:'row'}}>
-                    <ArrowText>FUTURE</ArrowText>
-                    <TouchableWithoutFeedback onPress={scrollRight}>
-                        <ArrowRightOutlined style={{color:'#42aaf5'}}/>
-                    </TouchableWithoutFeedback>
-                </View>
-            </ArrowNavigation>
-            <ScrollContainer>
-                {
-                    events.map((item,index)=>(
-                        <View key={index}>
-                            <Event
-                                style={{
-                                color:item.color,
-                                borderColor:item.color,
-                                flex:1,
-                                left:item.date[0][2]*10,
-                                borderRadius: 50,
-                                zIndex:1,
-                                }}
-                            >
-                                {item.event}
-                            </Event>
-                            <Stroke style={{width:(item.date[0][1]+5)*20}}></Stroke>
-                        </View>
-                    ))
-                }
-                <ScrollView
-                horizontal={true}
-                showsHorizontalScrollIndicator={false}
-                ref={scrollRef}
-                style={styles.scroller}
-                >
-                    <Container>
-                        {
-                            events.map((item,index)=>(
-                                <View style={{marginBottom:32,flexDirection:'column'}} key={index}>
-                                    <Line style={{left:(item.date[0][1]-1)*40, width:30*12*50, borderBottomColor:item.color}}></Line>
-                                    {
-                                        item.date.map((d)=>(
-                                            <Date
-                                            style={{
-                                                left:(30*d[0]+d[1]-1)*40,
-                                                backgroundColor:item.color,
-                                                borderRadius: 50,
-                                            }}
-                                            key={30*d[0]+d[1]}>
-                                                {d[1]}
-                                            </Date>
-                                        ))
-                                    }
-                                    <Fade style={{left:(item.date[0][1]-1)*40,width:(p-item.date[0][1])*40+20}}></Fade>
-                                    <Marker style={{left:(p-1)*40+20,zIndex:-1}}></Marker>
-                                </View>
-                            ))
-                        }
-                        <View style={{left:(p-1)*40+25}}><Text>Today {dayjs().date()}th {months[curr_month][1]}</Text></View>
-                        <View style={{flexDirection:'row'}}>
+            <MainContainer style = {styles.MainContainer} >
+                <ArrowNavigation>
+                    <View style={{flexDirection:'row'}}>
+                        <TouchableWithoutFeedback onPress={scrollLeft}>
+                            <ArrowLeftOutlined style={{color:'#42aaf5'}}/>
+                        </TouchableWithoutFeedback>
+                        <ArrowText>PAST</ArrowText>
+                    </View>
+                    <View style={{flexDirection:'row'}}>
+                        <ArrowText>FUTURE</ArrowText>
+                        <TouchableWithoutFeedback onPress={scrollRight}>
+                            <ArrowRightOutlined style={{color:'#42aaf5'}}/>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </ArrowNavigation>
+                <ScrollContainer>
+                    {
+                        events.map((item,index)=>(
+                            <View key={index}>
+                                <Event
+                                    style={{
+                                    color:item.color,
+                                    borderColor:item.color,
+                                    flex:1,
+                                    left:item.date[0][2]*10,
+                                    borderRadius: 50,
+                                    zIndex:1,
+                                    }}
+                                >
+                                    {item.event}
+                                </Event>
+                                <Stroke style={{width:(item.date[0][1]+5)*20}}></Stroke>
+                            </View>
+                        ))
+                    }
+                    <ScrollView
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    ref={scrollRef}
+                    style={styles.scroller}
+                    >
+                        <Container>
                             {
-                                months.map((m,index)=>(
-                                    <div key={index}>
-                                        <Months  style={{left:(m[0]*30)*40}}><Text>1st {m[1]}</Text></Months>
-                                    </div>
+                                events.map((item,index)=>(
+                                    <View style={{marginBottom:32,flexDirection:'column'}} key={index}>
+                                        <Line style={{left:(item.date[0][1]-1)*40, width:30*12*50, borderBottomColor:item.color}}></Line>
+                                        {
+                                            item.date.map((d)=>(
+                                                <Date
+                                                style={{
+                                                    left:(30*d[0]+d[1]-1)*40,
+                                                    backgroundColor:item.color,
+                                                    borderRadius: 50,
+                                                }}
+                                                key={30*d[0]+d[1]}>
+                                                    {d[1]}
+                                                </Date>
+                                            ))
+                                        }
+                                        <Fade style={{left:(item.date[0][1]-1)*40,width:(p-item.date[0][1])*40+20}}></Fade>
+                                        <Marker style={{left:(p-1)*40+20,zIndex:-1}}></Marker>
+                                    </View>
                                 ))
                             }
-                        </View>
-                    </Container>
-                </ScrollView>
-            </ScrollContainer>
+                            <View style={{left:(p-1)*40+25}}><Text>Today {dayjs().date()}th {months[curr_month][1]}</Text></View>
+                            <View style={{flexDirection:'row'}}>
+                                {
+                                    months.map((m,index)=>(
+                                        <div key={index}>
+                                            <Months  style={{left:(m[0]*30)*40}}><Text>1st {m[1]}</Text></Months>
+                                        </div>
+                                    ))
+                                }
+                            </View>
+                        </Container>
+                    </ScrollView>
+                </ScrollContainer>
+            </MainContainer>
       </>
   );
 }
