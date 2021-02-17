9c2cd42b5e077a194c949e1a98a35e04a51b1cdb
Ishita Keshawani <keshwaniishita054@gmail.com>
2021-02-17 15:46:29 +0530

    add files of build folder

---
+++ precache-manifest.0db226bc3319860737f8d5bfa5901f9c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86d05ac3f03262c0a17c15028567bbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "250b260cc793fc4a6538",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "df0b52d0e0cecdbf40b3",
+    "url": "./static/js/2.48d6caae.chunk.js"
+  },
+  {
+    "revision": "17ed30b50dfcd062a6b3fc57e7c14474",
+    "url": "./static/js/2.48d6caae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250b260cc793fc4a6538",
+    "url": "./static/js/main.3b76bc82.chunk.js"
+  },
+  {
+    "revision": "80cb98389f878c510cce",
+    "url": "./static/js/runtime-main.77c289f4.js"
+  },
+  {
+    "revision": "89b0e75122f5152b78ce6cdb991692e9",
+    "url": "./static/media/1.89b0e751.png"
+  },
+  {
+    "revision": "c42482cc4945db83451dadf62b948550",
+    "url": "./static/media/2.c42482cc.png"
+  },
+  {
+    "revision": "184c7dbd4662595e3e81b243851eb6ca",
+    "url": "./static/media/3.184c7dbd.png"
+  },
+  {
+    "revision": "e11bbee508ea4b3b9f44f4e0abb7b863",
+    "url": "./static/media/4.e11bbee5.png"
+  },
+  {
+    "revision": "7c53c6e80926bcf4993f2e6fa95b1b76",
+    "url": "./static/media/Frame 1.7c53c6e8.png"
+  },
+  {
+    "revision": "4e44d31d9d13d72cdf03685e1f8ae53d",
+    "url": "./static/media/about.4e44d31d.png"
+  },
+  {
+    "revision": "1587289c3419f2ce49affe85cafd2f50",
+    "url": "./static/media/code.1587289c.png"
+  },
+  {
+    "revision": "552c358973068e62c7a41bbbcce2fc67",
+    "url": "./static/media/contribute.552c3589.png"
+  },
+  {
+    "revision": "dc7f5974ca643775c44894377499a093",
+    "url": "./static/media/design_community.dc7f5974.png"
+  },
+  {
+    "revision": "9c56f1c3fe98a25d1039e4d23d14f895",
+    "url": "./static/media/documentation_training.9c56f1c3.png"
+  },
+  {
+    "revision": "ce74f5c458c825f4719a90fe59848e93",
+    "url": "./static/media/getting-started.ce74f5c4.png"
+  },
+  {
+    "revision": "3ce9c95b9f22a23441e6686eeb62ae6a",
+    "url": "./static/media/instagram_logo.3ce9c95b.png"
+  },
+  {
+    "revision": "1c27c6836b0f4ba7e6160f6e87e3f78c",
+    "url": "./static/media/landing.1c27c683.png"
+  },
+  {
+    "revision": "5a653ec391ddcff0753c57d08a3c2eb9",
+    "url": "./static/media/linkedin-logo.5a653ec3.png"
+  },
+  {
+    "revision": "45cc4ce0406ddd458e03a61f586ea483",
+    "url": "./static/media/logo.45cc4ce0.png"
+  },
+  {
+    "revision": "b19b61ad9e12c23fe0e4f6c34b9d1f69",
+    "url": "./static/media/our-projects.b19b61ad.png"
+  },
+  {
+    "revision": "1f92ca2215837d7a735516fa72246769",
+    "url": "./static/media/our_events.1f92ca22.png"
+  },
+  {
+    "revision": "66c6a83d8e650fe44c591ef600271c13",
+    "url": "./static/media/placeholder.66c6a83d.png"
+  },
+  {
+    "revision": "f4a562338e62d7a726cb5676d8c15dc8",
+    "url": "./static/media/project_management.f4a56233.png"
+  },
+  {
+    "revision": "e08739fb07642934b59d2ceb6d57a493",
+    "url": "./static/media/research_outreach.e08739fb.png"
+  },
+  {
+    "revision": "1d609eba9e9bfd2462f59af2dae9cb46",
+    "url": "./static/media/testing.1d609eba.png"
+  },
+  {
+    "revision": "b696dd8edc862a11b6568b99e3c7c17c",
+    "url": "./static/media/twitter_logo.b696dd8e.png"
+  }
+]);
