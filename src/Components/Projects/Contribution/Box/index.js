96001271ba6defc86f0cd1f9f130dc015aa06452
Bucky25 <65112369+Bucky25@users.noreply.github.com>
2021-02-18 22:03:29 +0530

    Minor tweaks

--- src/Components/Projects/Contribution/Box/index.js
+++ src/Components/Projects/Contribution/Box/index.js
@@ -5,26 +5,18 @@ import { StyleSheet, View} from 'react-native';
 const ContributionBox = ({ commitCount }) => {
     var hue = 0;
     switch (true) {
-        
-        case (commitCount < 2):
-            hue = .4;
-            break;
-        case (commitCount >=2 && commitCount < 4):
-            hue = .6;
-            break;
-        case (commitCount >=4 && commitCount < 8):
-            hue = .7;
-            break;
-        case (commitCount >=8 && commitCount < 12):
-            hue = .9;
-            break;
-        case (commitCount >=12 ):
-            hue = 1;
-            break;
-        default :
-            hue = 0;
-            break;    
+    if(commitCount < 2) {
+        hue = .4;
+    } else if (commitCount >=2 && commitCount < 4) {
+        hue = .6;
+    } else if (commitCount >=4 && commitCount < 8 ) {
+        hue = .7;
+    } else if (commitCount >=8 && commitCount < 12) {
+        hue = .9;
+    } else {
+        hue = 1;
     }
+        
     var colorCode = 'rgba(0, 113, 188,' + hue + ')';
     return (
         <View style={[style.box, {backgroundColor: colorCode}]}/>
