74c41716d6922a82f1bfb379c8f37bbfb05e8ad4
iamkryptonite <iamkryptonite@users.noreply.github.com>
2021-02-25 15:27:09 +0530

    fetched issues

--- src/Components/FirstTimeIssues/index.js
+++ src/Components/FirstTimeIssues/index.js
@@ -4,9 +4,44 @@ import SectionHeader from '../SectionHeader';
 import {TableHeader,TableHeaderText} from './style';
 import {CheckOutlined,InfoCircleOutlined,CaretDownOutlined} from '@ant-design/icons'
 export default class App extends Component {
-//   componentDidMount() {
-//     fetch('https://reactnative.dev/movies.json')
-//   }
+    state={};
+  componentDidMount() {
+    var repos=[],issues=[];
+    fetch('https://api.github.com/orgs/anitab-org/repos')
+    .then((response)=>response.json())
+    .then((json)=> json.forEach(function(repo,idx){
+        repos.push(repo.name);
+        fetch('https://api.github.com/repos/anitab-org/'+repo.name+'/issues')
+        .then((response)=>response.json())
+        .then((json)=>json.forEach(function(issue,idx){
+            var flag=false;
+            var issue_tmp={
+                url:issue.html_url,
+                title:issue.title,
+                milestones:issue.milestones,
+                comments:issue.comments,
+                number:issue.number,
+                assignees:issue.assignees,
+                labels:[],
+            };
+            issue.labels.forEach(function(label){
+                if(label.name==="First Timers Only")
+                    flag=true;
+                issue_tmp.labels.push({
+                    id:label.id,
+                    name:label.name,
+                    color:label.color
+                })
+            })
+            if(flag===true && issue_tmp!=null)
+                issues.push(issue_tmp)
+        }));
+    }))
+    .then(
+        this.setState({repos:issues}),
+        // console.log(this.state)
+    )
+  }
   render() {
     return (
       <View style={{alignItems: 'left',width:'80%'}}>
