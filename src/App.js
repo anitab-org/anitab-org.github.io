c46347686987b6ebee009d31882a1361e7ac4f7e
Brittany Joiner <brittanyjoiner15@gmail.com>
2021-10-18 17:46:12 -0500

    feat: Add routes and paths to nav bar items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
+import { useRoutes } from 'hookrouter';
+import Home from './Components/Home';
+import AboutUs from './Components/AboutUs';
 import Header from './Components/Header';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
+import Programs from './Components/Programs';
+import Projects from './Components/Projects';
+import Events from './Components/Events';
+import Contribute from './Components/Contribute';
 
 function App() {
   const [selected, setSelected] = useState(0);
@@ -14,10 +21,21 @@ function App() {
     'EVENTS',
     'CONTRIBUTE',
   ];
+
+  const routes = {
+    '/': () => <Home />,
+    '/about-us': () => <AboutUs />,
+    '/programs': () => <Programs />,
+    '/projects': () => <Projects />,
+    '/events': () => <Events />,
+    '/contribute': () => <Contribute />,
+  };
+
+  const routeResult = useRoutes(routes);
   return (
     <View style={{ position: 'absolute', width: '100%', alignItems: 'center' }}>
       <Header selected={selected} setSelected={setSelected} titles={titles} />
-      <Content selected={selected} titles={titles} />
+      {routeResult}
       <Footer />
     </View>
   );
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,7 +7,7 @@ import {
   StyleSheet,
 } from 'react-native';
 
-function Header({ selected, setSelected, titles }) {
+function Header({ selected, setSelected, titles, path }) {
   return (
     <View
       style={{
@@ -25,21 +25,23 @@ function Header({ selected, setSelected, titles }) {
         accessible={true}
         accessibilityLabel={titles[0]}
       >
-        <Image
-          style={{ height: 50, width: 100 }}
-          source={require('./../../assets/logo.png')}
-        />
+        <a styles={styles.links} href="/">
+          <Image
+            style={{ height: 50, width: 100 }}
+            source={require('./../../assets/logo.png')}
+          />
+        </a>
       </TouchableHighlight>
-      {MenuItem(1, selected, setSelected, titles[1])}
-      {MenuItem(2, selected, setSelected, titles[2])}
-      {MenuItem(3, selected, setSelected, titles[3])}
-      {MenuItem(4, selected, setSelected, titles[4])}
-      {MenuItem(5, selected, setSelected, titles[5])}
+      {MenuItem(1, selected, setSelected, titles[1], '/about-us')}
+      {MenuItem(2, selected, setSelected, titles[2], '/programs')}
+      {MenuItem(3, selected, setSelected, titles[3], '/projects')}
+      {MenuItem(4, selected, setSelected, titles[4], '/events')}
+      {MenuItem(5, selected, setSelected, titles[5], '/contribute')}
     </View>
   );
 }
 
-function MenuItem(index, selected, setSelected, title) {
+function MenuItem(index, selected, setSelected, title, path) {
   return (
     <TouchableHighlight
       style={styles.buttonContainer}
@@ -48,15 +50,18 @@ function MenuItem(index, selected, setSelected, title) {
       accessible={true}
       accessibilityLabel={title}
     >
-      <Text
-        style={{
-          borderBottomColor: selected === index ? 'powderblue' : 'transparent',
-          borderBottomWidth: 2,
-          alignSelf: 'center',
-        }}
-      >
-        {title}
-      </Text>
+      <a styles={styles.links} href={path}>
+        <Text
+          style={{
+            borderBottomColor:
+              selected === index ? 'powderblue' : 'transparent',
+            borderBottomWidth: 2,
+            alignSelf: 'center',
+          }}
+        >
+          {title}
+        </Text>
+      </a>
     </TouchableHighlight>
   );
 }
@@ -67,6 +72,12 @@ const styles = StyleSheet.create({
     marginRight: 70,
   },
   buttonContainer: { marginTop: 25, marginLeft: 40 },
+  links: {
+    textDecoration: 'none',
+    '&:visited': {
+      textDecoration: 'none',
+    },
+  },
 });
 
 export default Header;
