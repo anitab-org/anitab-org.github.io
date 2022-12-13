51d405ddc8102df9c2463bb39766349dc077e0d7
zainabnoori <zainab.noori@and.digital>
2022-12-13 15:30:16 +0000

    Create contributeIntro.js and replace it with contributeIntro.json in the index file

--- src/Components/Contribute/index.js
+++ src/Components/Contribute/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import SectionHeader from '../SectionHeader';
-import content from '../../content/contributeIntro.json';
+import content from '../../content/contributeIntro';
 import ImageContent from './../ImageContent';
 import { MainContainer, Box, Content, Description } from './style';
 import Hyperlink from 'react-native-hyperlink';
+import HTMLReactParser from 'html-react-parser';
 
 function Contribute() {
   const renderContent = () => {
@@ -19,7 +20,7 @@ function Contribute() {
                     linkStyle={{ color: '#2980b9' }}
                     onPress={(url) => window.open(url, '_blank')}
                   >
-                    <Description key={index}>{content.par}</Description>
+                    <Description key={index}>{HTMLReactParser(content.par)}</Description>
                   </Hyperlink>
                 );
               })}
